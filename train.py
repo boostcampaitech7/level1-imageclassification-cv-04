101989b780cd2b30e547e4720185504263274d49
HANSEONGLEE <leehs98712@gmail.com>
2024-09-21 14:14:51 +0900

    Feat: CLI add

--- train.py
+++ train.py
@@ -3,56 +3,48 @@ from torch.utils.data import DataLoader
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import torch.optim as optim
+import argparse
 
 from src.dataset import CustomDataset
 from src.transforms import TransformSelector
 from src.models import ModelSelector
 from src.trainer import Trainer, Loss
 
-def main():
+def main(args):
     # Set device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Load data
-    traindata_dir = "./data/train"
-    traindata_info_file = "./data/train.csv"
-    save_result_path = "./train_result"
-
-    train_info = pd.read_csv(traindata_info_file)
+    train_info = pd.read_csv(args.traindata_info_file)
     num_classes = len(train_info['target'].unique())
 
     # Split data
-    train_df, val_df = train_test_split(train_info, test_size=0.2, stratify=train_info['target'])
+    train_df, val_df = train_test_split(train_info, test_size=args.val_split, stratify=train_info['target'])
 
     # Set up transforms
-    transform_selector = TransformSelector(transform_type="albumentations")
+    transform_selector = TransformSelector(transform_type=args.transform_type)
     train_transform = transform_selector.get_transform(is_train=True)
     val_transform = transform_selector.get_transform(is_train=False)
 
     # Set up datasets
-    train_dataset = CustomDataset(root_dir=traindata_dir, info_df=train_df, transform=train_transform)
-    val_dataset = CustomDataset(root_dir=traindata_dir, info_df=val_df, transform=val_transform)
+    train_dataset = CustomDataset(root_dir=args.traindata_dir, info_df=train_df, transform=train_transform)
+    val_dataset = CustomDataset(root_dir=args.traindata_dir, info_df=val_df, transform=val_transform)
 
     # Set up dataloaders
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False)
 
     # Set up model
-    model_selector = ModelSelector(model_type='timm', num_classes=num_classes, model_name='resnet18', pretrained=True)
+    model_selector = ModelSelector(model_type=args.model_type, num_classes=num_classes, model_name=args.model_name, pretrained=args.pretrained)
     model = model_selector.get_model()
     model.to(device)
 
-    # 스케줄러 초기화
-    scheduler_step_size = 30  # 매 30step마다 학습률 감소
-    scheduler_gamma = 0.1  # 학습률을 현재의 10%로 감소
-    # 한 epoch당 step 수 계산
+    # Scheduler initialization
     steps_per_epoch = len(train_loader)
-    # 2 epoch마다 학습률을 감소시키는 스케줄러 선언
-    epochs_per_lr_decay = 2 
-    scheduler_step_size = steps_per_epoch * epochs_per_lr_decay
+    scheduler_step_size = steps_per_epoch * args.epochs_per_lr_decay
 
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step_size, gamma=scheduler_gamma)
+    optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step_size, gamma=args.scheduler_gamma)
 
     # Set up trainer and train
     trainer = Trainer(
@@ -63,10 +55,30 @@ def main():
         optimizer=optimizer,
         scheduler=scheduler,
         loss_fn=Loss(), 
-        epochs=5,
-        result_path=save_result_path
+        epochs=args.epochs,
+        result_path=args.save_result_path
     )
     trainer.train()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Train image classification model")
+    parser.add_argument("--traindata_dir", type=str, default="./data/train", help="Path to training data directory")
+    parser.add_argument("--traindata_info_file", type=str, default="./data/train.csv", help="Path to training data info file")
+    parser.add_argument("--save_result_path", type=str, default="./train_result", help="Path to save training results")
+    
+    parser.add_argument("--val_split", type=float, default=0.2, help="Validation split ratio")
+    parser.add_argument("--transform_type", type=str, default="albumentations", help="Type of data transforms to use")
+    parser.add_argument("--batch_size", type=int, default=64, help="Batch size for training and validation")
+
+    parser.add_argument("--model_type", type=str, default="timm", help="Type of model to use")
+    parser.add_argument("--model_name", type=str, default="resnet18", help="Name of the model")
+    parser.add_argument("--pretrained", type=bool, default=True, help="Whether to use pretrained weights")\
+    
+    parser.add_argument("--learning_rate", type=float, default=0.001, help="Initial learning rate")
+    parser.add_argument("--epochs_per_lr_decay", type=int, default=2, help="Number of epochs before learning rate decay")
+    parser.add_argument("--scheduler_gamma", type=float, default=0.1, help="Learning rate decay factor")
+
+    parser.add_argument("--epochs", type=int, default=5, help="Number of epochs to train")
+
+    args = parser.parse_args()
+    main(args)
