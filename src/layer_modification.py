816911939b2f79dd0b935cb8c2d969068febff5d
HANSEONGLEE <leehs98712@gmail.com>
2024-09-27 10:22:17 +0900

    EXP: Residual classification head
    fc mlp head를 rediual head로 변경

--- src/layer_modification.py
+++ src/layer_modification.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+
 def layer_modification(model):
     '''
     모델의 일부 layer만을 학습하거나, layer를 수정하기 위해 세팅하는 함수입니다
@@ -7,16 +8,33 @@ def layer_modification(model):
     for _, param in model.model.named_parameters():
         param.requires_grad = False
 
-    # Classification head만 학습 가능하게 설정
-        model.model.head = nn.Sequential(
-            nn.BatchNorm1d(1024),
-            nn.Linear(1024, 2048),
+    # Classification head를 residual 구조로 수정
+    model.model.head = ResidualHead(1024, 500)
+    model.model.head.requires_grad = True
+    return model
+
+class ResidualHead(nn.Module):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.block1 = nn.Sequential(
+            nn.BatchNorm1d(in_features),
+            nn.Linear(in_features, 2048),
             nn.GELU(),
             nn.BatchNorm1d(2048),
-            nn.Linear(2048, 1024),
+            nn.Linear(2048, in_features),
+            nn.GELU()
+        )
+        self.block2 = nn.Sequential(
+            nn.BatchNorm1d(in_features),
+            nn.Linear(in_features, 1024),
             nn.GELU(),
             nn.BatchNorm1d(1024),
-            nn.Linear(1024, 500)
+            nn.Linear(1024, in_features),
+            nn.GELU()
         )
-        model.model.head.requires_grad = True
-    return model
+        self.final = nn.Linear(in_features, out_features)
+
+    def forward(self, x):
+        x = x + self.block1(x)
+        x = x + self.block2(x)
+        return self.final(x)
